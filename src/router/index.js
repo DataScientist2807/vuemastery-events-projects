87af56b7b8e3beaa960b2c47443f698a8fd0eaac
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-09 20:39:00 +0200

    Finalize details of events

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '../views/EventList.vue'
-import AboutView from '../views/AboutView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "../views/EventList.vue";
+import AboutView from "../views/AboutView.vue";
+import EventDetails from "../views/EventDetails.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'EventList',
-    component: EventList
+    path: "/",
+    name: "EventList",
+    component: EventList,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: AboutView
-  }
-]
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
