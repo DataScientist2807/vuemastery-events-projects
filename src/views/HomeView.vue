e691ba02f6cd6043a5a909dfca20211b25d8c2da
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-09 16:05:26 +0200

    Setup home and EventCard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import EventCard from "@/components/EventCard.vue";
 
 export default {
-  name: 'HomeView',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 45454,
+          category: "animal welfare",
+          title: "Cat Adoption Day",
+          date: "January 28, 2022",
+          time: "12:00",
+        },
+        {
+          id: 23233,
+          category: "animal welfare 2",
+          title: "Cat Adoption Day 2",
+          date: "January 30, 2022",
+          time: "12:00",
+        },
+      ],
+    };
+  },
+};
 </script>
+<style scoped>
+  .events {
+    display: flex;
+    flex-direction: column;
+    align-items: center
+  }
+</style>
