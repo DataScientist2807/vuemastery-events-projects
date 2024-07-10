90da43d565195bf80ebe6dddf5e20162eefb4d4c
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-07-10 20:46:22 +0200

    Programmatic Routing

--- src/views/event/EventRegister.vue
+++ src/views/event/EventRegister.vue
@@ -1,10 +1,21 @@
 <template>
   <p>Register the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-props: ["event"],
+  props: ["event"],
+  methods: {
+    register() {
+      // Call to API
+      // If registered then redirect to event details
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 };
 </script>
 
